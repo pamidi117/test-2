06f57a0ccb3673fb2cf3c2b327b0abc21fe936ad
Praveen Chowdary pamidi <pamidip@mail.sacredheart.edu>
2024-01-30 22:49:05 -0500

    first commit

---
+++ Qeue/Qeue/Qeue.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+//DS Test 1
+
+// Function to display the elements of the queue
+void showq(queue<int> q) {
+    while (!q.empty()) {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+
+int main() {
+    queue<int> gquiz;
+
+    gquiz.push(10);
+    gquiz.push(20);
+    gquiz.push(30);
+
+    cout << "The queue gquiz is : ";
+    showq(gquiz);
+
+    cout << "\ngquiz.size() : " << gquiz.size();
+    cout << "\ngquiz.front() : " << gquiz.front();
+    cout << "\ngquiz.back() : " << gquiz.back();
+
+    cout << "\ngquiz.pop() : ";
+    gquiz.pop();
+    showq(gquiz);
+
+    return 0;
+}
